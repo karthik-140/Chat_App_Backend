5f30bc4d35cce350b12d0a50ff3905799dba17ab
karthik-140 <shanigaramkarthik@gmail.com>
2024-07-21 11:58:38 +0530

    added dialectModule in database to integrate with vercel

--- util/database.js
+++ util/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
     dialect: 'mysql',
-    host: process.env.HOST
+    host: process.env.HOST,
+    dialectModule: require('mysql2'),
 })
 
 module.exports = sequelize
