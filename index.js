edf38902e19f46a08dda05a2a837fe0fb6e24f65
karthik <kshanigaram@keypoint-tech.com>
2025-12-03 11:13:46 +0530

    dynamic server port setup based on dev/prod

--- index.js
+++ index.js
@@ -1,61 +1,68 @@
-const { instrument } = require('@socket.io/admin-ui');
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
-const cors = require('cors');
-const helmet = require('helmet');
-const http = require('http');
-const { Server } = require('socket.io')
-const cron = require('node-cron');
+const { instrument } = require("@socket.io/admin-ui");
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const cors = require("cors");
+const helmet = require("helmet");
+const http = require("http");
+const { Server } = require("socket.io");
+const cron = require("node-cron");
 
-const schedule = require('./util/schedule');
-const sequelize = require('./util/database');
-const Users = require('./models/users');
-const Messages = require('./models/messages');
-const Groups = require('./models/groups');
-const UserGroups = require('./models/userGroups');
+const schedule = require("./util/schedule");
+const sequelize = require("./util/database");
+const Users = require("./models/users");
+const Messages = require("./models/messages");
+const Groups = require("./models/groups");
+const UserGroups = require("./models/userGroups");
 
-const userRoutes = require('./routes/user');
-const messageRoutes = require('./routes/message');
-const groupRoutes = require('./routes/group');
+const userRoutes = require("./routes/user");
+const messageRoutes = require("./routes/message");
+const groupRoutes = require("./routes/group");
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://chat-app140.netlify.app',
-  methods: ["GET", "POST", "PUT", "DELETE"],
-}));
+const url =
+  process.env.MODE === "development"
+    ? process.env.FRONTEND_URL_DEV
+    : process.env.FRONTEND_HOST;
+
+app.use(
+  cors({
+    origin: url,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'https://chat-app140.netlify.app',
+    origin: url,
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
 
-io.on('connection', (socket) => {
-  socket.on('send-message', (message, userGroupInfo) => {
-    const groupId = userGroupInfo?.groupId
+io.on("connection", (socket) => {
+  socket.on("send-message", (message, userGroupInfo) => {
+    const groupId = userGroupInfo?.groupId;
     if (!groupId) {
-      socket.broadcast.emit('receive-message', (message))
+      socket.broadcast.emit("receive-message", message);
     } else {
-      socket.to(groupId).emit('receive-message', ({ ...userGroupInfo, message }))
+      socket.to(groupId).emit("receive-message", { ...userGroupInfo, message });
     }
-  })
+  });
 
-  socket.on('join-group', (groupId) => {
-    socket.join(groupId)
-  })
-})
+  socket.on("join-group", (groupId) => {
+    socket.join(groupId);
+  });
+});
 
 app.use(helmet());
 
 app.use(bodyParser.json({ extented: false }));
 
-app.use('/user', userRoutes);
-app.use('/messages', messageRoutes);
-app.use('/group', groupRoutes);
+app.use("/user", userRoutes);
+app.use("/messages", messageRoutes);
+app.use("/group", groupRoutes);
 
 Users.hasMany(Messages);
 Messages.belongsTo(Users);
@@ -71,14 +78,13 @@ instrument(io, { auth: false });
 sequelize
   .sync()
   .then(() => {
-    server.listen(process.env.PORT);
-  }).catch((err) => {
-    console.log(err)
+    server.listen(process.env.PORT || 4000);
   })
+  .catch((err) => {
+    console.log(err);
+  });
 
-cron.schedule('30 2 * * *', schedule.moveChatsToArchivedChats,
-  {
-    scheduled: true,
-    timezone: "Asia/Kolkata",
-  }
-);
+cron.schedule("30 2 * * *", schedule.moveChatsToArchivedChats, {
+  scheduled: true,
+  timezone: "Asia/Kolkata",
+});
